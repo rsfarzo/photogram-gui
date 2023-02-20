dc4e9f3c33b5e938b873f27eda1ae66b5eb7363b
Robert Sfarzo <sfarzo@uillinois.edu>
2023-02-20 20:26:08 +0000

    99%

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,8 +8,12 @@ class PhotosController < ApplicationController
     render({ :template => "photos/show.html.erb" })
   end
   def update
-    @photo= Photo.where(:id=>params.fetch(:id)).first
-    render({ :template => "photos/show.html.erb" })
+    photo= Photo.where(:id=>params.fetch(:id)).first
+    photo.image=params.fetch("input_image")
+    photo.caption=params.fetch("input_caption")
+    photo.save
+    redirect_to "/photos/#{photo.id}"
+    #render({ :template => "photos/show.html.erb" })
   end
   def create
     p=Photo.new
@@ -26,5 +30,12 @@ class PhotosController < ApplicationController
     Photo.destroy_by(id: id)
     redirect_to "/photos"
     #render({ :template => "photos/index.html.erb" })
+  end
+  def new_comment
+    pid=params.fetch("input_photo_id")
+    uid=params.fetch("input_author_id")
+    body=params.fetch("input_body")
+    Comment.create(:photo_id => pid, :author_id => uid, :body => body)
+    redirect_to "/photos/#{pid}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   get("/users/:username", { :controller => "users", :action => "show" })
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:id", { :controller => "photos", :action => "show" })
-  post("/update_photo/:id",{ :controller=>"photos", :action=>"update"})
+  get("/update_photo/:id",{ :controller=>"photos", :action=>"update"})
   get("/insert_user_record", {:controller=>"users",:action=>"new"})
   get("/update_user_record/:username", {:controller=>"users",:action=>"update"})
   get("/insert_photo_record", {:controller=>"photos",:action=>"create"})
   get("/delete_photo/:id",{:controller=>"photos",:action=>"delete"})
+  get("/insert_comment_record", {:controller=>"photos",:action=>"new_comment"})
 end
